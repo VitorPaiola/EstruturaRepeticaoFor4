07111c8eaa567f77917d99a20109a7a6a427e9f1
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-17 01:40:57 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaRepeticaoFor4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++) {
+    string[] vet = Console.ReadLine().Split(' ');
+    int x = int.Parse(vet[0]);
+    int y = int.Parse(vet[1]);
+
+    if (y == 0) {
+        Console.WriteLine("divisao impossivel");
+    } else {
+        double div = (double) x / y;
+        Console.WriteLine(div.ToString("F1", CultureInfo.InvariantCulture));
+    }
+}
